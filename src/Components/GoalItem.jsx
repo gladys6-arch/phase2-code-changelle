00e653a9b75867c282b69c7227fa95e63c3e6feb
gladys <achandogladys6@gmail.com>
2025-07-21 00:41:32 +0300

    add changes in GoalItem.jsx

--- src/Components/GoalItem.jsx
+++ src/Components/GoalItem.jsx
@@ -1,23 +1,40 @@
 import React from "react";
+import "./GoalItem.css"
 
 
 function GoalItem({goal, onDeleteGoal}){
   const { id, name, targetAmount, savedAmount, category, deadline } = goal;
+  const isCompleted = savedAmount >= targetAmount;
+  const progressPercent = Math.min((savedAmount / targetAmount) * 100, 100);
 
-  const daysLeft = Math.ceil(
-    (new Date(deadline) - new Date()) / (1000 * 60 * 60 * 24)
-  );
+  
+  const formattedDeadline = new Date(deadline).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
 
   return (
-    <li style={{border: "1px solid #ccc", padding: "10px"}}>
-      <h3>{name}</h3>
-      <p>Target Amount: ${targetAmount}</p>
-      <p>Saved Amount: ${savedAmount}</p>
-      <p>Category:{category}</p>
-      <p>Deadline: {deadline}</p>
-      <button onClick={()=> onDelete(id)}>Delete</button>
-    </li>
-  )
+    <div className="goal-card">
+      <h3 className="goal-name">{name}</h3>
+      <p className="goal-category"><strong>Category:</strong> {category}</p>
+      <p><strong>Target:</strong> KES {targetAmount}</p>
+      <p><strong>Saved:</strong> KES {savedAmount}</p>
+      <p><strong>Deadline:</strong> {formattedDeadline}</p>
+
+      <div className="progress-bar">
+        <div className="progress" style={{ width: `${progressPercent}%` }}>
+          {Math.floor(progressPercent)}%
+        </div>
+      </div>
+
+      {isCompleted && (
+        <p className="success">🎉 Goal completed!</p>
+      )}
+
+      <button className="delete-btn" onClick={() => onDeleteGoal(id)}>Delete</button>
+    </div>
+  );
 }
 
 export default GoalItem; 
