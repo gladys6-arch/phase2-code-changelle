8689e67ac516327f07892d2d251caf033698472a
gladys <achandogladys6@gmail.com>
2025-07-20 15:59:55 +0300

    add inputs in AddGoalForm.jsx

--- src/Components/AddGoalForm.jsx
+++ src/Components/AddGoalForm.jsx
@@ -36,14 +36,49 @@ function AddGoalForm({onAddGoal}){
       category:"",
       deadline:"",
       createdAt: new Date().toISOString().split("T")[0],
-    })
+    });
   }
 
 
   return (
-    <div>
 
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input
+      name="name"
+      placeholder="Goal Name"
+      value={formData.name}
+      onChange={handleChange}
+      />
+      <input
+      name="targetAmount"
+      type="number"
+      placeholder="Target"
+      value={formData.targetAmount}
+      onChange={handleChange}
+      />
+      <input
+      name="savedAmount"
+      type="number"
+      placeholder="Saved"
+      value={formData.savedAmount}
+      onChange={handleChange}
+
+      />
+      <input
+      name="category"
+      placeholder="Category"
+      value={formData.category}
+      onChange={handleChange}
+      />
+      <input
+       name="deadline"
+       type="date"
+       value={formData.deadline}
+       onChange={handleChange}
+      />
+      <button type="submit">Add goal</button>
+
+    </form>
   )
 }
 
