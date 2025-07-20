b9fb2299a1e73fa2ce771583de34407fee5f1d10
gladys <achandogladys6@gmail.com>
2025-07-20 20:12:23 +0300

    add new changes

--- src/Components/GoalItem.jsx
+++ src/Components/GoalItem.jsx
@@ -1,15 +1,14 @@
 
 
 function GoalItem({goal}){
+  const { name, targetAmount, savedAmount, deadline } = goal;
 
   return (
     <li style={{border: "1px solid #ccc", padding: "10px"}}>
-      <h4>{goal.name}</h4>
-      <p>Target: ${targetAmount}</p>
-      <p>Saved: ${savedAmount}</p>
+      <h3>{name}</h3>
+      <p>Target Amount: ${targetAmount}</p>
+      <p>Saved Amount: ${savedAmount}</p>
       <p>Deadline: {deadline}</p>
-      <p>Progress: {percentage} </p>
-
     </li>
   )
 }
