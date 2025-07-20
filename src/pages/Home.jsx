507a8fa80e26189225e402098b48030be2b6eb76
gladys <achandogladys6@gmail.com>
2025-07-20 14:11:58 +0300

    add new changes in Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+function Home(){
+  return (
+    <div>
+      <h1>Welcome to the Smart Goal Planner</h1>
+      <p>Track and achieve your savings goals!</p>
+    </div>
+  );
+}
+
+export default Home;
