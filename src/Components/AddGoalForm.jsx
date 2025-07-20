d21e8daa449cf0edae75a68da4cb111f662abdbb
gladys <achandogladys6@gmail.com>
2025-07-20 15:37:23 +0300

    add handleSubmit function

--- src/Components/AddGoalForm.jsx
+++ src/Components/AddGoalForm.jsx
@@ -9,7 +9,35 @@ function AddGoalForm({onAddGoal}){
     deadline:"",
     createdAt: new Date().toISOString().split("T")[0],
 
-  })
+  });
+
+  function handleChange(event){
+    const {name, value} = event.target;
+    setFormData ({...formData, [name]: value});
+
+  }
+
+  function handleSubmit(event){
+    event.preventDefault();
+
+    fetch("http://localhost:5000/goals",{
+      method: "POST",
+      headers:{"Content-Type": "application/json"},
+      body:JSON.stringify(formData),
+    
+    })
+    .then(res=>res.json())
+    .then(newGoal => onAddGoal(newGoal));
+
+    setFormData({
+      name:"",
+      targetAmount:"",
+      savedAmount:"",
+      category:"",
+      deadline:"",
+      createdAt: new Date().toISOString().split("T")[0],
+    })
+  }
 
 
   return (
